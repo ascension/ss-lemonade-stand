c77ce7de850382f7ccc9e927cee1cdf4dcaa8115
Jerrod Bunce <jerrod@engagereal.com>
2017-06-01 10:12:11 -0600

    Wire up Dashboard to route.

--- src/routes/LemonadeStand/index.js
+++ src/routes/LemonadeStand/index.js
@@ -1,10 +1,11 @@
 import AddressRoute from './routes/Address';
+import Dashboard from './containers/Dashboard';
 
 export default function LemonadeStandRoute(store) {
   return {
     path: 'dashboard',
     indexRoute: '',
-    component: '',
+    component: Dashboard,
     childRoutes: [
       AddressRoute()
     ]
