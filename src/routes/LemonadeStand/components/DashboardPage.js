4ba2b18cf2896f221810671c0825c4f0c5e45fb0
Jerrod Bunce <jerrod@engagereal.com>
2017-06-01 15:42:00 -0600

    Add Form for adding bitcoin address w/ memo and option to generate address. Added elements for showing addresses that are waiting for payments and those that have received payments.

--- src/routes/LemonadeStand/components/DashboardPage.js
+++ src/routes/LemonadeStand/components/DashboardPage.js
@@ -1,49 +1,216 @@
 import React, { Component, PropTypes } from 'react';
-import { Input } from '../../../components/Input';
+import { Link } from 'react-router';
+import { Input, HelpBlock } from '../../../components/Input';
 import { Button } from '../../../components/Buttons';
 import { bindClassMethods } from 'util/commonUtil';
 import { Container, Row } from 'components/Layout';
+import styled from 'styled-components';
+import { isValidBitcoinAddress } from 'util/bitcoinUtil';
 
 const propTypes = {
-  addresses: PropTypes.object
+  addresses: PropTypes.object,
+  createCustomerBitcoinAddress: PropTypes.func.isRequired
 };
+
+const H3 = styled.h3`
+    background: #f4f4f4;
+    border-top: 1px solid #ddd;
+    border-bottom: 1px solid #ddd;
+    padding: 10px 0;
+    margin-top: 0;
+    color: #888;
+    width: 100%;
+`;
+
+const Panel = styled.div`
+  margin-bottom: 20px;
+  background-color: #fff;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  -webkit-box-shadow: 0 1px 1px rgba(0,0,0,.05);
+  box-shadow: 0 1px 1px rgba(0,0,0,.05);
+  border-color: #ddd;
+`;
+
+const PanelHeading = styled.div`
+  padding: 10px 15px;
+  border-bottom: 1px solid transparent;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  color: #333;
+  background-color: #f5f5f5;
+  border-color: #ddd;
+`;
+
+const Stats = styled.p`
+  font-size: 18px;
+  font-weight: 700;
+  margin: 0;
+`;
+
+const PanelBody = styled.div`
+  padding: 15px;
+`;
+
+const CheckboxLabel = styled.label`
+  min-height: 20px;
+  margin-bottom: 0;
+  font-weight: 400;
+  cursor: pointer;
+  font-size: 14px;
+  & input {
+    margin-right: 10px;
+  }
+`;
 
 class DashboardPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      customerName: ''
+      memo: '',
+      bitcoinAddress: '',
+      generateNewAddress: false,
+      formErrors: {}
     };
     bindClassMethods.call(this, 'handleClick', 'handleChange');
   }
 
   handleClick(event) {
+    const { createCustomerBitcoinAddress } = this.props;
     event.preventDefault();
-    console.log(this.state)
-    // TODO - Call Action to create bitcoin address.
+
+    const formErrors = {};
+
+    const { memo, generateNewAddress, bitcoinAddress } = this.state;
+    if (memo === '') {
+      formErrors.memo = true;
+    }
+
+    if (!generateNewAddress && !isValidBitcoinAddress(bitcoinAddress)) {
+      formErrors.bitcoinAddress = true;
+    }
+
+    if (this.props.addresses[bitcoinAddress]) {
+      formErrors.uniqueBitcoinAddress = true;
+    }
+
+    if (Object.keys(formErrors).length > 0) {
+      this.setState({ formErrors });
+      return;
+    }
+
+    createCustomerBitcoinAddress({ memo, generateNewAddress, bitcoinAddress });
+    this.setState({
+      formErrors: {},
+      memo: '',
+      bitcoinAddress: '',
+      generateNewAddress: false
+    });
   }
 
   handleChange(event) {
     this.setState({
-      customerName: event.target.value
+      [event.target.name]: event.target.type === 'checkbox' ? event.target.checked : event.target.value
     });
   }
 
-  render() {
+  renderAddressTable(addresses) {
     return (
-    <Container>
-      <Row>
-        <div className="col-md-6">
-          Placeholder
-        </div>
-        <div className="col-md-6">
-        <form className="form">
-          <Input placeholder="Customer Name" value={this.state.customerName} onChange={this.handleChange}/>
-          <Button className="btn btn-default" onClick={this.handleClick}>Create Bitcoin Address</Button>
-        </form>
-        </div>
-      </Row>
-    </Container>
+      <table style={{ width: '100%' }}>
+        <tr style={{ borderBottom: '1px solid #ddd' }}>
+          <th style={{ color: '#999' }}>Bitcoin Address</th>
+          <th style={{ borderLeft: '1px solid #ddd', color: '#999' }}>Customer Name</th>
+        </tr>
+
+        {Object.values(addresses)
+          .reverse()
+          .map(({ publicAddress, memo }) => (
+            <tr><td><Link to={`/address/${publicAddress}`}>{publicAddress}</Link></td><td>{memo}</td></tr>
+          ))}
+      </table>
+    );
+  }
+
+  render() {
+    const { addresses } = this.props;
+
+    return (
+      <Container>
+        <Row>
+          <div className="col-md-6">
+            <Row>
+              <div className="col-md-6">
+                <Panel>
+                  <PanelHeading>Pending</PanelHeading>
+                  <PanelBody><Stats>Testing</Stats></PanelBody>
+                </Panel>
+              </div>
+              <div className="col-md-6">
+                <Panel>
+                  <PanelHeading>Pending</PanelHeading>
+                  <PanelBody><Stats>Testing</Stats></PanelBody>
+                </Panel>
+              </div>
+            </Row>
+            <Row>
+              <div className="col-md-6">
+                <Panel>
+                  <PanelHeading>Pending</PanelHeading>
+                  <PanelBody><Stats>Testing</Stats></PanelBody>
+                </Panel>
+              </div>
+              <div className="col-md-6">
+                <Panel>
+                  <PanelHeading>Pending</PanelHeading>
+                  <PanelBody><Stats>Testing</Stats></PanelBody>
+                </Panel>
+              </div>
+            </Row>
+          </div>
+          <div className="col-md-6">
+            <form className="form">
+              <div style={{ marginBottom: '30px' }}>
+                <Input placeholder="Memo" name="memo" value={this.state.memo} onChange={this.handleChange} />
+                {this.state.formErrors.memo && <HelpBlock hasError={true}>Memo is required.</HelpBlock>}
+              </div>
+              <div style={{ marginBottom: '30px' }}>
+                <Input
+                  style={{ marginBottom: '30px'}}
+                  placeholder={this.state.generateNewAddress ? '' : 'Bitcoin Address'}
+                  name="bitcoinAddress"
+                  value={!this.state.generateNewAddress ? this.state.bitcoinAddress : ''}
+                  onChange={this.handleChange}
+                  disabled={this.state.generateNewAddress}
+                />
+                {this.state.formErrors.bitcoinAddress &&
+                  <HelpBlock hasError={true}>A valid bitcoin address is required.</HelpBlock>}
+                {this.state.formErrors.uniqueBitcoinAddress &&
+                  <HelpBlock hasError={true}>Please enter unique bitcoin address.</HelpBlock>}
+              </div>
+              <div style={{ marginBottom: '30px', textAlign: 'left' }}>
+                <CheckboxLabel htmlFor="generateNewAddress">
+                  <input
+                    id="generateNewAddress"
+                    type="checkbox"
+                    name="generateNewAddress"
+                    onChange={this.handleChange}
+                    checked={this.state.generateNewAddress}
+                  />
+                  Generate Bitcoin Address
+                </CheckboxLabel>
+              </div>
+              <Button className="btn btn-default" onClick={this.handleClick}>Add Bitcoin Address</Button>
+            </form>
+          </div>
+        </Row>
+        <Row style={{ padding: '50px 0' }}>
+          <H3>Pending Payments</H3>
+          {addresses && Object.keys(addresses).length > 0 && this.renderAddressTable(addresses)}
+        </Row>
+        <Row>
+          <H3>Completed Payments</H3>
+        </Row>
+      </Container>
     );
   }
 }
