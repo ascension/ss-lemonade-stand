a58fe112f409624dcffa1067fd2111bb0affa96e
Jerrod Bunce <jerrod@engagereal.com>
2017-06-01 15:43:16 -0600

    Added HelpBlock for displaying text/error messages below inputs.

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -11,3 +11,11 @@ export const Input = styled.input.attrs({ className: 'form-control' })`
   box-shadow: 0 0 0 transparent;
   -webkit-appearance: none;
 `;
+
+export const HelpBlock = styled.p`
+  display: block;
+  margin-top: 5px;
+  margin-bottom: 10px;
+  color: ${props => props.hasError ? '#a94442' : '#737373' };
+  transition: all 200ms ease;
+`;
