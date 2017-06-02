5c978fea81caa4fc292f4c13204ed14f03eb097c
Jerrod Bunce <jerrod@engagereal.com>
2017-06-02 06:35:36 -0600

    Fix formatting and added missing semi-colons

--- src/main.js
+++ src/main.js
@@ -1,61 +1,54 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import createStore from './store/createStore'
-import './styles/main.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import createStore from './store/createStore';
+import './styles/main.scss';
 
 // ------------------------------------
 // Store Initialization
 // ------------------------------------
-const store = createStore(window.__INITIAL_STATE__)
+const store = createStore(window.__INITIAL_STATE__);
 
 // ------------------------------------
 // Render Setup
 // ------------------------------------
-const MOUNT_NODE = document.getElementById('lemonadeStandContainer')
+const MOUNT_NODE = document.getElementById('lemonadeStandContainer');
 
 let render = () => {
-  const App = require('./components/App').default
-  const routes = require('./routes/index').default(store)
+  const App = require('./components/App').default;
+  const routes = require('./routes/index').default(store);
 
-  ReactDOM.render(
-    <App store={store} routes={routes} />,
-    MOUNT_NODE
-  )
-}
+  ReactDOM.render(<App store={store} routes={routes} />, MOUNT_NODE);
+};
 
 // ------------------------------------
 // Load Development Tools
 // ------------------------------------
 if (__DEV__) {
   if (module.hot) {
-    const renderApp = render
-    const renderError = (error) => {
-      const RedBox = require('redbox-react').default
+    const renderApp = render;
+    const renderError = error => {
+      const RedBox = require('redbox-react').default;
 
-      ReactDOM.render(<RedBox error={error} />, MOUNT_NODE)
-    }
+      ReactDOM.render(<RedBox error={error} />, MOUNT_NODE);
+    };
 
     render = () => {
       try {
-        renderApp()
+        renderApp();
       } catch (e) {
-        console.error(e)
-        renderError(e)
+        console.error(e);
+        renderError(e);
       }
-    }
+    };
 
     // Setup hot module replacement
-    module.hot.accept([
-      './components/App',
-      './routes/index',
-    ], () =>
+    module.hot.accept(['./components/App', './routes/index'], () =>
       setImmediate(() => {
-        ReactDOM.unmountComponentAtNode(MOUNT_NODE)
-        render()
-      })
-    )
+        ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+        render();
+      }));
   }
 }
 
 // Render the App
-if (!__TEST__) render()
+if (!__TEST__) render();
--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,24 +1,43 @@
-import { applyMiddleware, compose, createStore as createReduxStore } from 'redux'
-import thunk from 'redux-thunk'
-import { browserHistory } from 'react-router'
-import makeRootReducer from './reducers'
-import { updateLocation } from './location'
+import { applyMiddleware, compose, createStore as createReduxStore } from 'redux';
+import thunk from 'redux-thunk';
+import { browserHistory } from 'react-router';
+import makeRootReducer from './reducers';
+import { updateLocation } from './location';
+import { coinCapSocketMiddleWare } from './bitcoinPrice';
 
 const createStore = (initialState = {}) => {
   // ======================================================
   // Middleware Configuration
   // ======================================================
-  const middleware = [thunk]
+  const blockchainSocket = new WebSocket('wss://ws.blockchain.info/inv');
+  blockchainSocket.onopen = event => {
+    // blockchainSocket.send(JSON.stringify({ "op": "unconfirmed_sub" }));
+    // Listen for messages
+    blockchainSocket.onmessage = function(event) {
+      console.log('Client received a message', event);
+    };
+
+    // Listen for socket closes
+    blockchainSocket.onclose = function(event) {
+      console.log('Client notified socket has closed', event);
+    };
+  };
+
+  // socket.connect('wss://ws.blockchain.info/inv', { path: '/'});
+  // blockchainSocket.on('connect_error', function (error) {
+  //   debugger;
+  // });
+  const middleware = [thunk, coinCapSocketMiddleWare];
 
   // ======================================================
   // Store Enhancers
   // ======================================================
-  const enhancers = []
-  let composeEnhancers = compose
+  const enhancers = [];
+  let composeEnhancers = compose;
 
   if (__DEV__) {
     if (typeof window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ === 'function') {
-      composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
     }
   }
 
@@ -28,25 +47,22 @@ const createStore = (initialState = {}) => {
   const store = createReduxStore(
     makeRootReducer(),
     initialState,
-    composeEnhancers(
-      applyMiddleware(...middleware),
-      ...enhancers
-    )
-  )
-  store.asyncReducers = {}
+    composeEnhancers(applyMiddleware(...middleware), ...enhancers)
+  );
+  store.asyncReducers = {};
 
   // Listen to browser history events and dispatch action
   // Invoke to un-subscribe
-  store.unsubscribeHistory = browserHistory.listen(updateLocation(store.dispatch))
+  store.unsubscribeHistory = browserHistory.listen(updateLocation(store.dispatch));
 
   if (module.hot) {
     module.hot.accept('./reducers', () => {
-      const reducers = require('./reducers').default
-      store.replaceReducer(reducers(store.asyncReducers))
-    })
+      const reducers = require('./reducers').default;
+      store.replaceReducer(reducers(store.asyncReducers));
+    });
   }
 
-  return store
-}
+  return store;
+};
 
-export default createStore
+export default createStore;
