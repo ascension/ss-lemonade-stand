0638df81e328408330eb0ba8549128ba64322e9b
Jerrod Bunce <jerrod@engagereal.com>
2017-06-02 08:12:52 -0600

    Update socketIoMiddleware to support subscribing to multiple channels.

--- src/util/middlewareUtil.js
+++ src/util/middlewareUtil.js
@@ -1,8 +1,11 @@
-export const createSocketIoMiddleware = (socket, channel, eventAction) => {
+export const createSocketIoMiddleware = (socket, channels = [], eventAction) => {
+  // TODO - Add ability to provide an array of channels
   return ({ dispatch }) => {
-    socket.on(channel, data => {
-      dispatch(eventAction(data));
-    });
+    for (let i = 0; i < channels.length; i++) {
+      socket.on(channels[i], data => {
+        dispatch(eventAction(data));
+      });
+    }
     return next => action => next(action);
   };
 };
