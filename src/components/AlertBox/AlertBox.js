8c6d918b98e83980a05dc70a853e5b097e3d0138
Jerrod Bunce <jerrod@engagereal.com>
2017-06-03 03:24:52 -0600

    Refactored to pull out functional and styled components from container.

---
+++ src/components/AlertBox/AlertBox.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Alert, AlertContainer } from './styled';
+import ClearIcon from 'react-icons/lib/md/clear';
+
+const propTypes = {
+  alerts: PropTypes.object,
+  deleteAlert: PropTypes.function.isRequired
+};
+
+const AlertBox = props => {
+  const { alerts, deleteAlert } = props;
+
+  return (
+    <AlertContainer>
+      {Object.values(alerts).slice(-5).map(alert => (
+        <Alert key={alert.id} type={alert.alertType}>
+          {alert.message}
+          <ClearIcon
+            onClick={() => deleteAlert(alert.id)}
+            style={{ height: '100%', fontSize: '28px', float: 'right', margin: 'auto 1em' }}
+          />
+        </Alert>
+      ))}
+    </AlertContainer>
+  );
+};
+
+AlertBox.propTypes = propTypes;
+
+export default AlertBox;
--- src/containers/Alerts.js
+++ src/containers/Alerts.js
@@ -1,52 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
-import ClearIcon from 'react-icons/lib/md/clear';
 import { deleteAlert } from 'store/middleware/alerts';
-
-const AlertBox = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 1000;
-`;
-
-const alertColors = { info: 'blue', success: '#dff0d8', error: '#DB0304' };
-
-const Alert = styled.div`
-  font-size: 14px;
-  width: 100%;
-  color: #fff;
-  background-color: #dff0d8;
-  border-color: #d0e9c6;
-  color: #3c763d;
-  background: ${props => alertColors[props.type]};
-  height: 75px;
-  display: block;
-  text-align: center;
-  z-index: 1000;
-  line-height: 75px;
-  transition: all 500ms ease;
-`;
-
-const Alerts = props => {
-  const { alerts, deleteAlert } = props;
-
-  return (
-    <AlertBox>
-      {Object.values(alerts).slice(-5).map(alert => (
-        <Alert key={alert.id} type={alert.alertType}>
-          {alert.message}
-          <ClearIcon
-            onClick={() => deleteAlert(alert.id)}
-            style={{ height: '100%', fontSize: '28px', float: 'right', margin: 'auto 1em' }}
-          />
-        </Alert>
-      ))}
-    </AlertBox>
-  );
-};
+import { AlertBox } from 'components/AlertBox';
 
 const mapDispatchToProps = {
   deleteAlert
@@ -58,4 +13,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Alerts);
+export default connect(mapStateToProps, mapDispatchToProps)(AlertBox);
