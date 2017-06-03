567ff76f2c00c4d5c1683cf561b51f6103e22cfd
Jerrod Bunce <jerrod@engagereal.com>
2017-06-03 10:18:50 -0600

    Update coinCap middleware to handle connection errors and reconnects and dispatches appropriate actions.

--- src/store/middleware/coinPrices.js
+++ src/store/middleware/coinPrices.js
@@ -5,6 +5,9 @@ import { createSocketIoMiddleware } from 'util/middlewareUtil';
 // Constants
 // ------------------------------------
 export const UPDATE_COIN_PRICE = 'UPDATE_COIN_PRICE';
+export const COIN_PRICE_CONNECTION_ERROR = 'COIN_PRICE_CONNECTION_ERROR';
+export const COIN_PRICE_RECONNECTING = 'COIN_PRICE_RECONNECTING';
+export const COIN_PRICE_RECONNECT_SUCCESS = 'COIN_PRICE_RECONNECT_SUCCESS';
 
 // ------------------------------------
 // Selectors
@@ -14,12 +17,25 @@ export const getBtcPrice = state => parseFloat(state.coinPrices.BTC.price) || 0;
 // ------------------------------------
 // Actions
 // ------------------------------------
-export const updateCoinPrice = data => {
-  return {
-    type: UPDATE_COIN_PRICE,
-    payload: data
-  };
-};
+export const updateCoinPrice = data => ({
+  type: UPDATE_COIN_PRICE,
+  payload: data
+});
+
+export const coinPriceConnectionError = error => ({
+  type: COIN_PRICE_CONNECTION_ERROR,
+  payload: error
+});
+
+export const coinPriceReconnecting = attemptNum => ({
+  type: COIN_PRICE_RECONNECTING,
+  payload: { attemptNum }
+});
+
+export const coinPriceReconnectSuccess = attemptNum => ({
+  type: COIN_PRICE_RECONNECT_SUCCESS,
+  payload: { attemptNum }
+});
 
 export const updateBtcPrice = ({ message: { btcPrice, ...restOfMessage } }) => {
   return updateCoinPrice({ ...restOfMessage, price: btcPrice, coin: 'BTC' });
@@ -34,6 +50,10 @@ export default function coinPricesReducer(state = initialState, action) {
     case UPDATE_COIN_PRICE:
       const { payload: { coin, ...coinInfo } } = action;
       return { ...state, [coin]: coinInfo };
+    case COIN_PRICE_CONNECTION_ERROR:
+      return { ...state, connectionError: true };
+    case COIN_PRICE_RECONNECTING:
+      return { ...state, reconnecting: true };
     default:
       return state;
   }
@@ -43,5 +63,10 @@ export default function coinPricesReducer(state = initialState, action) {
 // ------------------------------------
 // Middleware
 // ------------------------------------
-const socket = io.connect('http://socket.coincap.io');
-export const coinCapSocketMiddleWare = createSocketIoMiddleware(socket, ['global'], updateBtcPrice);
+
+export const coinCapSocketMiddleWare = createSocketIoMiddleware('http://socket.coincap.io', ['global'], {
+  onMessage: updateBtcPrice,
+  onConnectError: coinPriceConnectionError,
+  onReconnecting: coinPriceReconnecting,
+  onReconnect: coinPriceReconnectSuccess
+});
--- src/util/middlewareUtil.js
+++ src/util/middlewareUtil.js
@@ -1,11 +1,34 @@
-export const createSocketIoMiddleware = (socket, channels = [], eventAction) => {
+import io from 'socket.io-client';
+
+export const createSocketIoMiddleware = (
+  url,
+  channels = [],
+  { onMessage, onConnectError, onReconnecting, onReconnect }
+) => {
   // TODO - Add ability to provide an array of channels
+  const socket = io.connect(url);
+
   return ({ dispatch }) => {
+    socket.on('connect_failed', function(error) {});
+
+    socket.on('connect_error', function(error) {
+      dispatch(onConnectError(error));
+    });
+
+    socket.on('reconnect', function(attemptNum) {
+      dispatch(onReconnect(attemptNum));
+    });
+
+    socket.on('reconnecting', function(attemptNum) {
+      dispatch(onReconnecting(attemptNum));
+    });
+
     for (let i = 0; i < channels.length; i++) {
       socket.on(channels[i], data => {
-        dispatch(eventAction(data));
+        dispatch(onMessage(data));
       });
     }
+
     return next => action => next(action);
   };
 };
